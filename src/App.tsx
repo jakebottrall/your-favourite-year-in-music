e7ffd849e135b269a4e47660ef934f2a0b93ffee
jakebottrall <jakebottrall@gmail.com>
2023-01-15 14:24:50 -0800

    setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import { Callback } from './routes/Callback';
+import { Home } from './routes/Home';
+import { Login } from './routes/Login';
+
 export const App = () => {
-  return <div></div>;
+  return (
+    <div className='h-screen w-screen bg-black text-white'>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/callback' element={<Callback />} />
+      </Routes>
+    </div>
+  );
 };
---
+++ src/routes/Callback.tsx
@@ -0,0 +1,3 @@
+export const Callback = () => {
+  return <div>Callback</div>;
+};
