513ef3c89c719e36c059990c3c7a2b72cb2c54e0
jakebottrall <jakebottrall@gmail.com>
2023-01-15 22:03:26 -0800

    deploy to github pages v4

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='your-favourite-year-in-music'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
